f6b355492d8e25bc3cd922e1ec38ee9277c40c3d
combrige <combrige47@gmail.com>
2026-07-06 11:54:48 +0800

    批量修改

--- parse/pdf2md.py
+++ parse/pdf2md.py
@@ -1,9 +1,24 @@
+from pathlib import Path
+
 import opendataloader_pdf
 
-opendataloader_pdf.convert(
-    input_path=[
-        "2026-03-31_000488_ST晨鸣_2025年年度报告.pdf"
-    ],
-    output_dir="open_output",
-    format="markdown"
-)
+class Pdf2Md:
+    def __init__(self):
+        backend:str = 'openpdf'
+    def parse(self,input_path:str,output_path:str):
+        opendataloader_pdf.convert(
+            input_path=[input_path,],
+            output_dir=output_path,
+            format="markdown"
+        )
+    def batch_parse(self,input_path:str,output_path:str):
+        input_path_obj = Path(input_path)
+        opendataloader_pdf.convert(
+            input_path=[str(input_path_obj),],
+            output_dir=output_path,
+            format="markdown"
+        )
+
+if __name__ == '__main__':
+    pdf2md = Pdf2Md()
+    pdf2md.batch_parse("../安道麦A","test_output")
